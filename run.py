a35cc7d889fa2ec0c2710602c31bb1d9b9edbcc4
vvvadim <v.vladimirov@it-park.tech>
2023-01-24 19:13:40 +0300

    Initial commit

---
+++ run.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, render_template
+from utils import *
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main_page():
+    return render_template('main_page.html')
+
+
+@app.route('/movie/title')
+def movie_title_page():
+    title = request.args.get('title')
+    movie = search_by_title(title)
+    return render_template('movie_title_page.html', movie=movie)
+
+
+@app.route('/movie/year_to_year')
+def movies_release_year_page():
+    from_year = request.args.get('from_year')
+    to_year = request.args.get('to_year')
+    movies = search_by_years(int(from_year), int(to_year))
+    return render_template('movies_release_year_page.html', movies=movies)
+
+
+@app.route('/rating/<rating>')
+def rating_children_page(rating):
+    if rating == 'children':
+        rating = ('G', 'TV-G')
+    elif rating == 'family':
+        rating = ('G', 'PG', 'PG-13')
+    elif rating == 'adult':
+        rating = ('R', 'NC-17')
+    movies = search_by_rating(rating)
+    return render_template('movies_rating_page.html', movies=movies)
+
+
+
+@app.route('/genre/genre')
+def movie_genre_page():
+    genre = request.args.get('genre')
+    movies = search_by_genre(genre)
+    return render_template('movies_genre_page.html', movies=movies)
+
+
+@app.errorhandler(404)
+def not_found_page(e):
+    return '<h1>404 Error</h1>'
+
+
+@app.errorhandler(500)
+def not_found_page(e):
+    return '<h1>500 Error</h1>'
+
+if __name__ == '__main__':
+    app.run()
---
+++ utils.py
@@ -0,0 +1,127 @@
+import sqlite3
+
+
+def search_by_title(title: str) -> dict[str, str | int]:
+    with sqlite3.connect("database/netflix.db") as connection:
+        cursor = connection.cursor()
+        sqlite_query = """
+        SELECT title, country, release_year, listed_in, description 
+        FROM netflix 
+        WHERE title = ? 
+        ORDER BY date_added DESC 
+        LIMIT 1
+        """
+        result = cursor.execute(sqlite_query, (title,)).fetchone()
+        return {
+            "title": result[0],
+            "country": result[1],
+            "release_year": result[2],
+            "genre": result[3],
+            "description": result[4].strip()
+        }
+
+
+def search_by_years(f_year: int, t_year: int) -> list[dict[str, str | int]]:
+    with sqlite3.connect("database/netflix.db") as connection:
+        cursor = connection.cursor()
+        sqlite_query = """
+        SELECT title, release_year
+        FROM netflix
+        WHERE release_year BETWEEN ? AND ?
+        LIMIT 100
+        """
+        result = cursor.execute(sqlite_query, (f_year, t_year)).fetchall()
+        movies = []
+        for row in result:
+            dict_ = {
+                "title": row[0],
+                "release_year": row[1]
+            }
+            movies.append(dict_)
+        return movies
+
+
+
+def search_by_rating(rating: str):
+    with sqlite3.connect("database/netflix.db") as connection:
+        cursor = connection.cursor()
+        sqlite_query = f"""
+        SELECT title, rating, description
+        FROM netflix
+        WHERE rating IN {rating}
+        LIMIT 20
+        """
+        result = cursor.execute(sqlite_query).fetchall()
+        movies = []
+        for row in result:
+            dict_ = {
+                "title": row[0],
+                "rating": row[1],
+                "description": row[2]
+            }
+            movies.append(dict_)
+        return movies
+
+
+def search_by_genre(genre: str) -> list[dict[str, str | int]]:
+    with sqlite3.connect("database/netflix.db") as connection:
+        cursor = connection.cursor()
+        sqlite_query = """
+        SELECT title, description
+        FROM netflix
+        WHERE listed_in = ?
+        ORDER BY release_year DESC
+        LIMIT 10
+        """
+        result = cursor.execute(sqlite_query, (genre,)).fetchall()
+        movies = []
+        for row in result:
+            dict_ = {
+                "title": row[0],
+                "description": row[1]
+            }
+            movies.append(dict_)
+        return movies
+
+
+def get_actors(actor1: str, actor2: str):
+    with sqlite3.connect("database/netflix.db") as connection:
+        connection.row_factory = sqlite3.Row
+        cursor = connection.cursor()
+        sqlite_query = """
+        SELECT "cast"
+        FROM netflix
+        WHERE "cast" LIKE ? AND "cast" LIKE ?
+        GROUP BY "cast"
+        """
+        result = cursor.execute(sqlite_query, ('%' + actor1 + '%', '%' + actor2 + '%')).fetchall()
+        actors = []
+        dict_ = {}
+        for row in result:
+            for actor_names in row:
+                for i in actor_names.split(', '):
+                    dict_[i] = 0
+                for i in actor_names.split(', '):
+                    dict_[i] += actor_names.split(', ').count(i)
+        for k, v in dict_.items():
+            if k != actor1 and k != actor2:
+                if v > 2:
+                    actors.append(k)
+        return actors
+
+
+
+def get_movies_by_chars(type_: str, release_year: int, genre: str):
+    with sqlite3.connect("database/netflix.db") as connection:
+        cursor = connection.cursor()
+        sqlite_query = """
+        SELECT title
+        FROM netflix
+        WHERE type = ? AND release_year = ? AND listed_in = ?
+        LIMIT 20
+        """
+        result = cursor.execute(sqlite_query, (type_, release_year, genre)).fetchall()
+        return [row[0] for row in result]
+
+
+print(get_actors("Jack Black", "Dustin Hoffman"))
